ef543de44f7507269188285218583ae257388d9f
Felipe Salazar <codfelsociety@gmail.com>
2017-06-28 22:45:34 -0300

    Atualização de interface região superior
    Recolocado o menu padrão bootstrap com pesquisa, separação do css tema/cor do bootstrap(classes).

--- js/csNavbar.js
+++ js/csNavbar.js
@@ -1,17 +1,13 @@
 ﻿$(document).ready(function () {
     $(window).scroll(function () {
-        //if you hard code, then use console
-        //.log to determine when you want the 
-        //nav bar to stick.  
-        i = 0;
         var b = $('#top_area').height();
-        console.log($(window).scrollTop())
         if ($(window).scrollTop() > b) {
-            $('#nav_bar').addClass('navbar-fixed-top').fadeIn(1000);
-            i = 1;
+            $('#nav_bar').addClass('fixed-top');
+          //  $('#mini_logo').removeClass('hidden');
         }
         if ($(window).scrollTop() < b + 1 ) {
-            $('#nav_bar').removeClass('navbar-fixed-top');
+            $('#nav_bar').removeClass('fixed-top');
+          //  $('#mini_logo').addClass('hidden');
         }
     });
 });
